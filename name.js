0e166afc850a7cd051d9f55190b73820e9ef000e
RicardoCavalheiro123 <up202005103@fe.up.pt>
2022-12-19 18:19:37 +0000

    my projects

--- name.js
+++ name.js
@@ -1,6 +1,6 @@
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml7 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter bluish'>$&</span>");
 
 anime.timeline({loop: false})
   .add({
@@ -14,41 +14,71 @@ anime.timeline({loop: false})
     delay: (el, i) => 50 * i
   });
 
-  let phrase = "Full-Stack Developer!";
-  let targetEl = document.getElementById("hover1");
-  phrase = phrase.split("").map((letter, index) => {
-      let el = document.createElement("span");
-      el.innerHTML = letter;
-      el.setAttribute("data-index", index.toString());
-      el.classList.add("hover-char");
-      targetEl.appendChild(el);
+let phrase = "Software Engineer";
+let targetEl = document.getElementById("hover1");
+phrase = phrase.split("").map((letter, index) => {
+    let el = document.createElement("span");
+    el.innerHTML = letter;
+    el.setAttribute("data-index", index.toString());
+    el.classList.add("hover-char");
+    targetEl.appendChild(el);
 
-  });
+});
 
-  let removeClasses = () => {
-      hoverChars.map((char) => {
-          char.classList.remove("hovered");
-          char.classList.remove("hovered-adjacent");
-      });
+let removeClasses = () => {
+    hoverChars.map((char) => {
+        char.classList.remove("hovered");
+        char.classList.remove("hovered-adjacent");
+        char.classList.remove("bluish");
+    });
 
-  }
-  let hoverChars = [...document.getElementsByClassName("hover-char")];
-  hoverChars.map((char) => {
-      char.addEventListener("mouseover", (e) => {
-          removeClasses();
-          let currentElement = e.target;
-          let index = parseInt(currentElement.getAttribute("data-index"));
-          let prevIndex = index === 0 ? null : index - 1;
-          let nextIndex = index === phrase.length - 1 ? null : index + 1;
-          let prevEl = prevIndex !== null && document.querySelector('span[data-index="'+prevIndex+'"]');
+}
+let hoverChars = [...document.getElementsByClassName("hover-char")];
+hoverChars.map((char) => {
+    char.addEventListener("mouseover", (e) => {
+        removeClasses();
+        let currentElement = e.target;
+        let index = parseInt(currentElement.getAttribute("data-index"));
+        let prevIndex = index === 0 ? null : index - 1;
+        let nextIndex = index === phrase.length - 1 ? null : index + 1;
+        let prevEl = prevIndex !== null && document.querySelector('span[data-index="'+prevIndex+'"]');
 
-          let nextEl = nextIndex !== null && document.querySelector('span[data-index="'+nextIndex+'"]');
-          e.target.classList.add("hovered");
-          prevEl && prevEl.classList.add("hovered-adjacent");
-          nextEl && nextEl.classList.add("hovered-adjacent");
-      });
-      
-  });
-  document.getElementById("hover1").addEventListener("mouseleave", (e) => {
-      removeClasses();
-  });
+        let nextEl = nextIndex !== null && document.querySelector('span[data-index="'+nextIndex+'"]');
+        e.target.classList.add("hovered");
+        e.target.classList.add("bluish");
+        prevEl && prevEl.classList.add("hovered-adjacent");
+        nextEl && nextEl.classList.add("hovered-adjacent");
+    });
+    
+});
+document.getElementById("hover1").addEventListener("mouseleave", (e) => {
+    removeClasses();
+});
+
+
+
+
+function colorImage(){
+    let photo = document.getElementById("profileimage");
+
+    //when you hover over the image, animation to change color
+    photo.onmouseover = function(){
+        photo.style.filter = "grayscale(0%)";
+        //zoom in on the image
+        photo.style.transform = "scale(1.2)";
+        photo.style.transition = "all 0.3s ease-in-out";
+
+
+
+    }
+    //when you hover out of the image, it will turn to grayscale
+    photo.onmouseout = function(){
+        photo.style.filter = "grayscale(50%)";
+        //zoom out of the image
+        photo.style.transform = "scale(1)";
+
+    }
+}
+colorImage();
+
+
